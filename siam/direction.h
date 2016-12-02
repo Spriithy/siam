c9593c4f2c0a4353fd5f7d82a7d72d4f40007ed7
Spriithy <theo.dano@gmail.com>
2016-12-02 18:00:51 +0100

    dzoih

--- siam/direction.h
+++ siam/direction.h
@@ -7,18 +7,4 @@ using namespace std;
 
 enum Direction { HAUT, BAS, DROITE, GAUCHE };
 
-string afficherDirection(Direction d) {
-  switch (d) {
-    case HAUT:
-      return "^";
-    case BAS:
-      return "v";
-    case DROITE:
-      return ">";
-    case GAUCHE:
-      return "<";
-  }
-  return "?";
-}
-
 #endif  // DIRECTION_H
--- siam/pion.cpp
+++ siam/pion.cpp
@@ -1,8 +1,27 @@
 #include "pion.h"
+
 Pion::Pion() {
   this->d = HAUT;
   this->type = VIDE;
 }
 
 void Pion::pivoter(Direction d) { this->d = d; }
-string Pion::texte() { return " " + afficherDirection(this->d); }
+string Pion::texte() {
+  string s = " ";
+  switch (this->d) {
+    case HAUT:
+      s += "^";
+      break;
+    case BAS:
+      s += "v";
+      break;
+    case DROITE:
+      s += ">";
+    case GAUCHE:
+      s += "<";
+    default:
+      s += "?";
+      break;
+  }
+  return s;
+}
