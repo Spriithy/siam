2e02748addfa58f298139469e104910bac7be1e8
brieucjan <janbrieuc@gmail.com>
2016-12-02 20:03:18 +0100

    up manager

--- siam/manager.cpp
+++ siam/manager.cpp
@@ -4,3 +4,21 @@ Manager::Manager()
 {
 
 }
+
+void Pion() {
+int x,y;
+cout<<"Vous avez choisi de rentrer un pion sur la grille"<<endl;
+cout<<"Indiquez les coordonnées voulues"<<endl;
+cout<<"Quelle colonne? ( 1,2,3,4 ou 5)"<<endl;
+cin>>x;
+cout<<"Quelle ligne? ( 1,2,3,4 ou 5)"<<endl;
+cin>>y;
+
+if ((x<0) || (x>5)|| (y<0) || (y>5) )
+{
+    cout<<"les coordonnées choisies sont erronées"<<endl;
+}
+
+else
+
+}
